781787d1b65dff8f6f4ebae764d126d57af31d33
ZikiiXu <47014540+ZikiiXu@users.noreply.github.com>
2019-05-08 00:58:25 -0400

    Update Final.js

--- Final.js
+++ Final.js
@@ -1,3 +1,9 @@
-$("picture").click(function(){
-	$("ironman").hide(1000);
-});
+$(document).ready(function(){
+  	$("#video").click(function(){
+  		$("#div1").alert("There is no video Duh- Da~");
+  	});
+  	$("h1").moiseover(function(){
+  		$("h1").css("background-color", "pink")
+  	});
+
+  });
