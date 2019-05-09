bd623fac93192f5cb277c5897b402e60a485e2d1
ZikiiXu <47014540+ZikiiXu@users.noreply.github.com>
2019-05-09 18:28:34 -0400

    Update Final.js

--- Final.js
+++ Final.js
@@ -1,9 +1,6 @@
 $(document).ready(function(){
   	$("#video").click(function(){
-  		$("#div1").alert("There is no video Duh- Da~");
-  	});
-  	$("h1").moiseover(function(){
-  		$("h1").css("background-color", "pink")
+  		alert("There is no video Duh- Da~");
   	});
 
   });
